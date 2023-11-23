660977a76e2fcaa31888ebb438d79badb190f77a
amurrai <andre.murrai@gmail.com>
2023-11-23 17:48:22 -0500

    add function to search for word vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,10 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+// function that returns the transposed matrix as a new array
+const transpose = function(matrix) {
+  let matrix2 = [];
+  matrix[0].map(() => matrix2.push([]));
+  matrix.map((element, i) =>
+    matrix[i].map((element, j) =>
+      matrix2[j][i] = matrix[i][j]));
+  return matrix2;
+};
 
-    return false
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  // transposing and joining horizontally to search using same method as above
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
